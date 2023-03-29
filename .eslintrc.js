8d53a0620f830e1216c69e5290510bc709658812
Jackize <94910747+Jackize@users.noreply.github.com>
2023-03-29 15:17:38 +0700

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -43,7 +43,8 @@ module.exports = {
     "arrow-spacing": [
       "error", { "before": true, "after": true }
     ],
-    "no-console": "error",
+    "no-console": 0,
+    "process": 0,
     "react/prop-types": 0
   }
 }
